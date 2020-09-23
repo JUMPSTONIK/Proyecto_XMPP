9cd5eda9e862f6cd4013af9c69e2baba958bf3da
JUMPSTONIK <xtineen@gmail.com>
2020-09-23 03:53:39 -0600

    menu del usuario creado

--- Client.py
+++ Client.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-    SleekXMPP: The Sleek XMPP Library
-    Copyright (C) 2011  Nathanael C. Fritz
-    This file is part of SleekXMPP.
-    See the file LICENSE for copying permission.
-"""
-
 import sys
 import logging
 import getpass
-import threading
 from optparse import OptionParser
 
 import sleekxmpp
 from sleekxmpp.exceptions import IqError, IqTimeout
-
 
 # Python versions before 3.0 do not use UTF-8 encoding
 # by default. To ensure that Unicode is handled properly
@@ -29,140 +19,95 @@ else:
     raw_input = input
 
 
-class RosterBrowser(sleekxmpp.ClientXMPP):
-
-    """
-    A basic script for dumping a client's roster to
-    the command line.
-    """
+class RegisterBot(sleekxmpp.ClientXMPP):
 
     def __init__(self, jid, password):
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
-        # The session_start event will be triggered when
-        # the bot establishes its connection with the server
-        # and the XML streams are ready for use. We want to
-        # listen for this event so that we we can initialize
-        # our roster. We need threaded=True so that the
-        # session_start handler doesn't block event processing
-        # while we wait for presence stanzas to arrive.
         self.add_event_handler("session_start", self.start, threaded=True)
-        self.add_event_handler("changed_status", self.wait_for_presences)
-
-        self.received = set()
-        self.presences_received = threading.Event()
+        self.add_event_handler("register", self.register, threaded=True)
 
     def start(self, event):
-        """
-        Process the session_start event.
-        Typical actions for the session_start event are
-        requesting the roster and broadcasting an initial
-        presence stanza.
-        Arguments:
-            event -- An empty dictionary. The session_start
-                     event does not provide any additional
-                     data.
-        """
-        try:
-            self.get_roster()
-        except IqError as err:
-            print('Error: %s' % err.iq['error']['condition'])
-        except IqTimeout:
-            print('Error: Request timed out')
+        
         self.send_presence()
-
-
-        print('Waiting for presence updates...\n')
-        self.presences_received.wait(5)
-
-        print('Roster for %s' % self.boundjid.bare)
-        groups = self.client_roster.groups()
-        for group in groups:
-            print('\n%s' % group)
-            print('-' * 72)
-            for jid in groups[group]:
-                sub = self.client_roster[jid]['subscription']
-                name = self.client_roster[jid]['name']
-                if self.client_roster[jid]['name']:
-                    print(' %s (%s) [%s]' % (name, jid, sub))
-                else:
-                    print(' %s [%s]' % (jid, sub))
-
-                connections = self.client_roster.presence(jid)
-                for res, pres in connections.items():
-                    show = 'available'
-                    if pres['show']:
-                        show = pres['show']
-                    print('   - %s (%s)' % (res, show))
-                    if pres['status']:
-                        print('       %s' % pres['status'])
-
+        self.get_roster()
         self.disconnect()
 
-    def wait_for_presences(self, pres):
-        """
-        Track how many roster entries have received presence updates.
-        """
-        self.received.add(pres['from'].bare)
-        if len(self.received) >= len(self.client_roster.keys()):
-            self.presences_received.set()
-        else:
-            self.presences_received.clear()
+    def register(self, iq):
+        
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['register']['username'] = self.boundjid.user
+        resp['register']['password'] = self.password
 
+        try:
+            resp.send(now=True)
+            logging.info("Account created for %s!" % self.boundjid)
+        except IqError as e:
+            logging.error("Could not register account: %s" %
+                    e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            logging.error("No response from server.")
+            self.disconnect()
 
 
 if __name__ == '__main__':
-    # Setup the command line arguments.
-    optp = OptionParser()
-    optp.add_option('-q','--quiet', help='set logging to ERROR',
-                    action='store_const',
-                    dest='loglevel',
-                    const=logging.ERROR,
-                    default=logging.ERROR)
-    optp.add_option('-d','--debug', help='set logging to DEBUG',
-                    action='store_const',
-                    dest='loglevel',
-                    const=logging.DEBUG,
-                    default=logging.ERROR)
-    optp.add_option('-v','--verbose', help='set logging to COMM',
-                    action='store_const',
-                    dest='loglevel',
-                    const=5,
-                    default=logging.ERROR)
+    st_log = False
 
-    # JID and password options.
-    optp.add_option("-j", "--jid", dest="jid",
-                    help="JID to use")
-    optp.add_option("-p", "--password", dest="password",
-                    help="password to use")
-    opts,args = optp.parse_args()
+    while st_log == False:
+        print("Bienvenido al chat basado en XMPP \n Por favor elija alguna de las siguientes opcions para empezar: \n 1 - Iniciar sesion \n 2 - Registrarse \n 3 - salir del chat\n")
+        eleccion = input("opcion: ")
+        if eleccion == '1':
+            #codigo para iniciar sesion
 
-    # Setup logging.
-    logging.basicConfig(level=opts.loglevel,
-                        format='%(levelname)-8s %(message)s')
+            if xmpp.connect():
+                xmpp.process(block=True)
+                access = True
+                print("login Listo")
+                while access:
+                    print("elija una de las siguientes opciones: \n1. Mostrar todos los usuarios y su estado \n2. Agregar un usuario a sus contactos \n3. Mostrar detalles de contacto de un usuario \n4. Comunicacion 1 a 1 con algun usuario \n5. participar en conversacion grupal \n6. salir ")
+                    opcion = input("opcion a elegir es: ")
+                    if opcion == "1":
+                        #codigo para mostrar usuarios
+                    if opcion == "2":
+                        #codigo para agregar contanto
+                    if opcion == "3":
+                        #codigo para mostrar detalles de un contanto
+                    if opcion == "4":
+                        #codigo para iniciar conversacion 1 a 1 con otro usuario
+                    if opcion == "5":
+                        #codigo para añadirme a un chat grupal
+                    if opcion == "6":
+                        xmpp.disconnect()
+                        access = False
+                    if opcion != "1" or opcion != "2" or opcion != "3" or opcion != "4" or opcion != "5" or opcion != "6": 
+                        print("ha escrito mala su eleccion. Por favor, intentelo y sin dejar espacios en blanco")
+            else:
+                print("Unable to connect.")
 
-    if opts.jid is None:
-        opts.jid = raw_input("Username: ")
-    if opts.password is None:
-        opts.password = getpass.getpass("Password: ")
+        if eleccion == '2':
+            #codigo para registrarse
+            # Setup the RegisterBot and register plugins. Note that while plugins may
+            # have interdependencies, the order in which you register them does
+            # not matter.
+            userJID = input("userJID: ")
+            password = input("password: ")
+            xmpp = RegisterBot(userJID, password)
 
-    xmpp = RosterBrowser(opts.jid, opts.password)
+            # Some servers don't advertise support for inband registration, even
+            # though they allow it. If this applies to your server, use:
+            xmpp['xep_0077'].force_registration = True
+            xmpp.register_plugin('xep_0030') # Service Discovery
+            xmpp.register_plugin('xep_0004') # Data forms
+            xmpp.register_plugin('xep_0066') # Out-of-band Data
+            xmpp.register_plugin('xep_0077') # In-band Registration
+            if xmpp.connect():
+                xmpp.process(block=True)
+            else:
+                print("Unable to connect.")
+            xmpp.disconnect()
+        if eleccion == '3':
+            st_log = True
+        if eleccion != '1' or eleccion != '2' or eleccion '3':
+            print("Por favor, solo elija el numero de las opciones que se le ha proporcionado sin dejar espacios\n")
 
-    # If you are working with an OpenFire server, you may need
-    # to adjust the SSL version used:
-    # xmpp.ssl_version = ssl.PROTOCOL_SSLv3
-
-    # If you want to verify the SSL certificates offered by a server:
-    # xmpp.ca_certs = "path/to/ca/cert"
-
-    # Connect to the XMPP server and start processing XMPP stanzas.
-    if xmpp.connect():
-        # If you do not have the dnspython library installed, you will need
-        # to manually specify the name of the server if it does not match
-        # the one in the JID. For example, to use Google Talk you would
-        # need to use:
-        #
-        # if xmpp.connect(('talk.google.com', 5222)):
-        #     ...
-        xmpp.process(block=True)
-    else:
-        print("Unable to connect.")
