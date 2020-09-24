093c76f3b58dd15c4eb11863b55fe248df874bf5
JUMPSTONIK <xtineen@gmail.com>
2020-09-23 18:24:00 -0600

    ya funciona registro y creando el login

--- Client.py
+++ Client.py
@@ -5,28 +5,30 @@ import logging
 import getpass
 from optparse import OptionParser
 
-import sleekxmpp
-from sleekxmpp.exceptions import IqError, IqTimeout
+import asyncio
+import slixmpp
+from slixmpp.exceptions import IqError, IqTimeout
 
 # Python versions before 3.0 do not use UTF-8 encoding
 # by default. To ensure that Unicode is handled properly
-# throughout SleekXMPP, we will set the default encoding
+# throughout slixmpp, we will set the default encoding
 # ourselves to UTF-8.
+'''
 if sys.version_info < (3, 0):
-    from sleekxmpp.util.misc_ops import setdefaultencoding
+    from slixmpp.util.misc_ops import setdefaultencoding
     setdefaultencoding('utf8')
 else:
     raw_input = input
+'''
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-
-class RegisterBot(sleekxmpp.ClientXMPP):
+class RegisterBot(slixmpp.ClientXMPP):
 
     def __init__(self, jid, password):
-        print(jid)
-        print(password)
-        sleekxmpp.ClientXMPP.__init__(self, jid, password)
-        self.add_event_handler("session_start", self.start, threaded=True)
-        self.add_event_handler("register", self.register, threaded=True)
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+        self.add_event_handler("session_start", self.start)
+        self.add_event_handler("register", self.register)
 
     def start(self, event):
         
@@ -34,7 +36,7 @@ class RegisterBot(sleekxmpp.ClientXMPP):
         self.get_roster()
         self.disconnect()
 
-    def register(self, iq):
+    async def register(self, iq):
         
         resp = self.Iq()
         resp['type'] = 'set'
@@ -42,7 +44,7 @@ class RegisterBot(sleekxmpp.ClientXMPP):
         resp['register']['password'] = self.password
 
         try:
-            resp.send(now=True)
+            await resp.send()
             print("Account created for %s!" % self.boundjid)
         except IqError as e:
             print("Could not register account: %s" %
@@ -52,16 +54,16 @@ class RegisterBot(sleekxmpp.ClientXMPP):
             print("No response from server.")
             self.disconnect()
 
-class Client(sleekxmpp.ClientXMPP):
+class Client(slixmpp.ClientXMPP):
 
     def __init__(self, jid, password):
-        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+        slixmpp.ClientXMPP.__init__(self, jid, password)
         self.add_event_handler("session_start", self.start)
         self.add_event_handler("message", self.message)
 
-    def start(self, event):
+    async def start(self, event):
         self.send_presence()
-        self.get_roster()
+        await self.get_roster()
 
     def message(self, msg):
         if msg['type'] in ('chat', 'normal'):
@@ -78,13 +80,13 @@ if __name__ == '__main__':
             #codigo para iniciar sesion
             userJID = input("userJID: ")
             password = input("password: ")
-            xmpp = Client(userJID, password)
+            xmpp = Client(userJID + "@redes2020.xyz", password)
             xmpp.register_plugin('xep_0030') # Service Discovery
             xmpp.register_plugin('xep_0004') # Data Forms
             xmpp.register_plugin('xep_0060') # PubSub
             xmpp.register_plugin('xep_0199') # XMPP Ping
             if xmpp.connect():
-                xmpp.process(block=True)
+                xmpp.process(block=False)
                 access = True
                 print("login Listo")
                 while access:
@@ -121,8 +123,6 @@ if __name__ == '__main__':
             userJID = input("userJID: ")
             password = input("password: ")
 
-            print(userJID)
-            print(password)
             xmpp = RegisterBot(userJID + "@redes2020.xyz", password)
             
             # Some servers don't advertise support for inband registration, even
@@ -133,11 +133,8 @@ if __name__ == '__main__':
             xmpp.register_plugin('xep_0066') # Out-of-band Data
             xmpp.register_plugin('xep_0077') # In-band Registration
             xmpp['xep_0077'].force_registration = True
-            if xmpp.connect():
-                xmpp.process(block=True)
-            else:
-                print("Unable to connect.")
-            xmpp.disconnect()
+            xmpp.connect()
+            xmpp.process()
         if eleccion == '3':
             userJID = input("userJID: ")
             password = input("password: ")
@@ -147,7 +144,7 @@ if __name__ == '__main__':
             xmpp.register_plugin('xep_0060') # PubSub
             xmpp.register_plugin('xep_0199') # XMPP Ping
             if xmpp.connect():
-                xmpp.process(block=True)
+                xmpp.process(block=False)
                 xmpp.del_roster_item(userJID + "@redes2020.zxy")
                 xmpp.disconnect()
             else:
